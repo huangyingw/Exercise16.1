8c90ff5b5d2a0e91c9609f443ebdb3a73e263593
huangyingw@linux_programming <huangyingw@gmail.com>
2012-07-18 19:37:22 -0500

    n

--- src/inheritance/Book.java
+++ src/inheritance/Book.java
@@ -1,5 +1,8 @@
 package inheritance;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Book extends Product {
 	private String title;
 }
--- src/inheritance/DVD.java
+++ src/inheritance/DVD.java
@@ -1,5 +1,8 @@
 package inheritance;
 
+import javax.persistence.Entity;
+
+@Entity
 public class DVD extends Product {
 	private String genre;
 }
